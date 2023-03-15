4d91ed3a352825ea8d4a37e6f32c9c70c5541515
Wahaco1 <103980648+Wahaco1@users.noreply.github.com>
2023-03-15 21:15:44 +0500

    Update sql.sql

--- MyWebApp/src/main/resources/sql.sql
+++ MyWebApp/src/main/resources/sql.sql
@@ -1,12 +1,4 @@
 create database myDB;
-use myDB;
-
-create table users (id integer auto_increment primary key,
-  `email` VARCHAR(45) NOT NULL,
-  `first name` VARCHAR(45) NOT NULL,
-  `last name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(15) NOT NULL,
-  `enabled` BIT(1) NOT NULL);
 
 
 
